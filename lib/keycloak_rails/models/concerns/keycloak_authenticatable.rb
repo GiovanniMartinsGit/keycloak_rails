e5a3cbf675173854b9c82657ab7db7d932ea84db
Felipe Vale <felipepaivavale@gmail.com>
2026-06-25 14:56:57 -0300

    config para config de cpf para cidadao

--- lib/keycloak_rails/models/concerns/keycloak_authenticatable.rb
+++ lib/keycloak_rails/models/concerns/keycloak_authenticatable.rb
@@ -24,6 +24,26 @@ module KeycloakRails
           update!(keycloak_id: keycloak_sub)
         end
 
+        
+        def assign_attributes_from_keycloak(user_info, scope = :default)
+          config = KeycloakRails.configuration(scope)
+          
+          self.keycloak_id = user_info["sub"] if self.respond_to?(:keycloak_id=)
+
+          if config.model_email_field.present? && self.respond_to?("#{config.model_email_field}=")
+            self.send("#{config.model_email_field}=", user_info["email"]) if self.send(config.model_email_field).blank?
+          end
+
+          if config.model_name_field.present? && self.respond_to?("#{config.model_name_field}=")
+            self.send("#{config.model_name_field}=", user_info["name"] || user_info["preferred_username"]) if self.send(config.model_name_field).blank?
+          end
+
+          if config.model_cpf_field.present? && self.respond_to?("#{config.model_cpf_field}=")
+            cpf_value = user_info["cpf"] || user_info["preferred_username"]
+            self.send("#{config.model_cpf_field}=", cpf_value) if self.send(config.model_cpf_field).blank? && cpf_value.present?
+          end
+        end
+
         def unlink_keycloak!
           update!(keycloak_id: nil)
         end
--- lib/keycloak_rails/services/user_resolver_service.rb
+++ lib/keycloak_rails/services/user_resolver_service.rb
@@ -3,7 +3,7 @@
 module KeycloakRails
   module Services
     class UserResolverService < BaseService
-      def call(user_info)
+            def call(user_info)
         # Log temporário com todos os dados retornados pelo provedor (Keycloak/Gov.br)
         log_info("=== DADOS EXTRAÍDOS DO GOV.BR/KEYCLOAK ===")
         log_info(user_info.inspect)
@@ -11,13 +11,13 @@ module KeycloakRails
 
         email = user_info["email"]
         keycloak_id = user_info["sub"]
+        cpf_value = user_info["cpf"] || user_info["preferred_username"]
 
-        raise UserNotFoundError.new("Dados do usuário incompletos", user_info: user_info) if email.blank?
         raise UserNotFoundError.new("Dados do usuário incompletos", user_info: user_info) if keycloak_id.blank?
 
         log_info("Resolvendo usuário na aplicação")
 
-        user = find_user(email)
+        user = find_user(email, cpf_value)
 
         if user.nil? && config.create_user_on_first_login
           user = create_user(email, keycloak_id, user_info)
@@ -31,8 +31,26 @@ module KeycloakRails
 
       private
 
-      def find_user(email)
-        config.resource_model.find_by(config.model_email_field => email)
+      def find_user(email, cpf_value = nil)
+        user = nil
+        
+        # Tenta buscar por CPF se a configuração existir e houver um CPF
+        if config.model_cpf_field.present? && cpf_value.present?
+          # Remove formatações caso seja um CPF válido para buscar adequadamente
+          cpf_limpo = cpf_value.gsub(/\D/, '')
+          if cpf_limpo.length == 11 || cpf_limpo.length == 14
+             user = config.resource_model.where("REPLACE(REPLACE(REPLACE(#{config.model_cpf_field}, '.', ''), '-', ''), '/', '') = ?", cpf_limpo).first
+          else
+             user = config.resource_model.find_by(config.model_cpf_field => cpf_value)
+          end
+        end
+
+        # Se não encontrar por CPF, tenta por e-mail, como fallback
+        if user.nil? && email.present?
+          user = config.resource_model.find_by(config.model_email_field => email)
+        end
+
+        user
       end
 
       def create_user(email, keycloak_id, user_info)
